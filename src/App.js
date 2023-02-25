a43f2f76d6740087c1432d1d744435b5b915197c
Rahul Jain <rahulcom64@gmail.com>
2023-02-25 22:43:34 +0530

    Removed extra files

--- src/App.js
+++ src/App.js
@@ -3,14 +3,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './style/main.css'
 import './style/index.scss'
 
-import { NavigationBar } from "./components/NavigationBar"
 import { HomeBanner } from './components/HomeBanner';
 import { Footer } from './components/Footer';
 
 export const App = () => {
   return (
     <>
-      {/* <NavigationBar /> */}
       <HomeBanner/>
       <Footer/>
     </>
