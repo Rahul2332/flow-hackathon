d72aeca5253dfc028799d9fe31350bc47706b29d
Anurag Singh Naruka <Anuragsinghnaruka@gmail.com>
2023-03-01 07:26:48 +0530

    user signup transaction

--- src/components/UserSignup.js
+++ src/components/UserSignup.js
@@ -8,18 +8,69 @@ export const UserSignup = () => {
     const [user, setUser] = useState({ loggedIn: null })
     const navigate = useNavigate();
 
+    const contractName = "AngelFlow";
+    const adminAddress = "0x9f78a1504db85885";
+
     useEffect(() => {
         fcl.currentUser.subscribe(setUser)
       }, []);
 
     useEffect(() => {
-        if(user.loggedIn)
+        if(user.loggedIn){
+            createTrader();
+
             navigate("/");
+        }
+        const createTrader = async () => {
+            const transactionId = await fcl.mutate({
+                cadence: `
+                    import ${contractName} from ${adminAddress}
+                    
+                    transaction {
+                        prepare(acct: AuthAccount) 
+                        {
+                            let vaultA <- ${contractName}.createVault(amount: 0.0)
+
+                            let TraderA = ${contractName}.createTrader(tId: 123, tName: "User1", tWallet: 0xeb14454b4855b22f)
+                            
+                            acct.save<${contractName}.Trader>(TraderA, to: /storage/MainTrader)
+                            acct.save<@${contractName}.Vault>(<-vaultA, to: /storage/MainVault)
+                            log("Empty Vault stored")
+                        
+                            acct.link<&${contractName}.Vault{${contractName}.Receiver, ${contractName}.Balance}>(/public/MainReceiver, target: /storage/MainVault)
+                            log("Public Receiver reference created!")
+
+                            acct.link<&${contractName}.Trader{${contractName}.TraderProfile}>(/public/TraderProfile, target: /storage/MainTrader)
+                            log("Public TraderProfile reference created!")
+                    
+                        }
+                        post
+                        {
+                            getAccount(${user.addr}).getCapability<&${contractName}.Vault{${contractName}.Receiver}>(/public/MainReceiver)
+                            .check():
+                            "Vault Receiver Reference was not created correctly!"
+                        
+                            getAccount(${user.addr}).getCapability<&${contractName}.Trader{${contractName}.TraderProfile}>(/public/TraderProfile)
+                            .check():
+                            "Trader Profile Reference was not created correctly!"
+                        }
+                    }
+                `,
+                // args: (arg, t) => [arg("Apple",t.String), arg("apple@gmail.com",t.String), arg(1,t.UInt64), arg(user.addr,t.Address), arg(30.0,t.UFix64), arg(1.0,t.UFix64)],
+                payer: fcl.authz,
+                proposer: fcl.authz,
+                authorizations: [fcl.authz],
+                limit: 50,
+            });
+            const transaction = await fcl.tx(transactionId).onceSealed();
+            console.log(transaction);
+        }
     }, [user]);
 
-    const login = () => {
-        if(!user.loggedIn)
+    const signup = () => {
+        if(!user.loggedIn){
             fcl.logIn()
+        }
     }
 
     return (
@@ -34,10 +85,7 @@ export const UserSignup = () => {
                             <div class="mb-3">
                                 <input type="text" class="form-control" id="companyName" placeholder="User Name" />
                             </div>
-                            <Button onClick={login} className="container fw-bold my-3" variant='contained' style={{backgroundColor:'#9467fe',textTransform:'capitalize'}}>Sign Up</Button>
-
-                            <p className="mt-5">Already Registered ?</p>
-                            <Button onClick={()=>{navigate("/signup")}} className="container fw-bold mb-3" variant='outlined' style={{borderColor:'#9467fe', color:'#9467fe',textTransform:'capitalize'}}>Login</Button>
+                            <Button onClick={signup} className="container fw-bold my-3" variant='contained' style={{backgroundColor:'#9467fe',textTransform:'capitalize'}}>Sign Up</Button>
                         </div>
                     </div>
                 </div>
